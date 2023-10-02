f9f175a23b99a98200837ab8b9f060620cf97a0b
sozi <sozyarnadir@gmail.com>
2023-10-03 00:29:33 +0300

    Changes to the homepag

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,32 +1,44 @@
 import React, { useState } from "react";
-import Button from "@mui/material/Button";
+import img from "/public/herosection.jpg";
+
 const Home = () => {
   let [isLoggedIn, setLogIn] = useState(true);
 
   return (
-    <div className="h-full bg-blue-50 text-blue-900">
-      <header className="p-4 shadow-lg bg-blue-900 text-white rounded-lg">
+    <div
+      className="h-full bg-blue-50 text-blue-900 font-sans"
+      style={{ fontFamily: "Raleway, sans-serif" }}
+    >
+      <header
+        className="p-4 shadow-lg bg-white text-black rounded-lg sticky top-0 z-50"
+        style={{ zIndex: 1000 }} // Ensuring the navbar stays on top of other elements
+      >
         <nav className="container mx-auto flex justify-between items-center rounded-lg">
           <div className="flex items-center">
-            <h1 className="text-4xl font-bold mr-8 rounded-lg">Name</h1>
-            <ul className="flex space-x-6 text-lg">
+            <h1 className="text-2xl font-bold mr-8 rounded-lg hover:custom-shadow transition-shadow duration-300">
+              Booking
+            </h1>
+            <ul className="flex space-x-4 text-sm">
               <li>
                 <a
                   href="#home"
-                  className="hover:underline rounded-lg px-2 py-1"
+                  className="no-underline hover:custom-shadow transition-shadow duration-300 rounded-lg px-2 py-1"
                 >
                   Home
                 </a>
               </li>
               <li>
-                <a href="#map" className="hover:underline rounded-lg px-2 py-1">
+                <a
+                  href="#map"
+                  className="no-underline hover:custom-shadow transition-shadow duration-300 rounded-lg px-2 py-1"
+                >
                   Map
                 </a>
               </li>
               <li>
                 <a
                   href="#accommodations"
-                  className="hover:underline rounded-lg px-2 py-1"
+                  className="no-underline hover:custom-shadow transition-shadow duration-300 rounded-lg px-2 py-1"
                 >
                   Accommodations
                 </a>
@@ -34,24 +46,24 @@ const Home = () => {
             </ul>
           </div>
 
-          <div className="flex items-center space-x-6 text-lg">
-            <button className="text-white hover:text-blue-300 rounded-full p-2">
+          <div className="flex items-center space-x-4 text-sm">
+            <button className="text-blue-900 hover:text-blue-300 hover:custom-shadow transition-shadow duration-300 rounded-full p-2">
               <i className="fas fa-search"></i>
             </button>
-            <select className="p-2 bg-white text-blue-900 rounded-full border">
+            <select className="p-2 bg-white text-blue-900 rounded-full border hover:custom-shadow transition-shadow duration-300">
               <option>English</option>
               <option>Spanish</option>
             </select>
             {isLoggedIn ? (
               <button
-                className="bg-white text-blue-900 p-2 rounded-full hover:bg-blue-100 border"
+                className="bg-white text-blue-900 p-2 rounded-full hover:bg-blue-100 border hover:custom-shadow transition-shadow duration-300"
                 onClick={() => setLogIn(false)}
               >
                 Logout
               </button>
             ) : (
               <button
-                className="bg-white text-blue-900 p-2 rounded-full hover:bg-blue-100 border"
+                className="bg-white text-blue-900 p-2 rounded-full hover:bg-blue-100 border hover:custom-shadow transition-shadow duration-300"
                 onClick={() => setLogIn(true)}
               >
                 Login
@@ -60,9 +72,52 @@ const Home = () => {
           </div>
         </nav>
       </header>
+
+      <div className="h-[60vh] flex justify-center items-center">
+        <div className="container relative w-full h-[45vh] flex flex-col justify-center items-center rounded-3xl mx-14  ">
+          <img
+            src={img}
+            alt="Scenic View"
+            className="my-image w-full h-full object-cover rounded-3xl bg-bottom" // Added custom class for styling
+          />{" "}
+          {/* <button
+            className="absolute top-0 right-0 bg-white p-2 rounded"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+          >
+            Hover me
+          </button> */}
+          <div className=" absolute bottom-4 bg-white rounded-full p-3 flex  ">
+            <ul className="flex justify-center text-black container  ">
+              <li className="flex flex-col w-[250px] items-center">
+                Location
+                <span className="opacity-60	">Where are you going?</span>
+              </li>{" "}
+              <li className="flex flex-col w-[250px] items-center ">
+                Check in
+                <span className="opacity-60	">Add date</span>
+              </li>
+              <li className="flex flex-col w-[250px] items-center border-x-2 border-black">
+                {" "}
+                Check out
+                <span className="opacity-60	">Add date</span>
+              </li>
+              <li className="flex flex-col w-[250px] items-center">
+                {" "}
+                Guest
+                <span className="opacity-60	">Number of guests</span>
+              </li>
+              <div className="overlay w-full ">
+                <span id="span"> Welcome to jurasic park</span>
+              </div>
+            </ul>
+          </div>
+        </div>
+      </div>
+
       <div className="flex justify-center items-center h-screen">
         <h1 className="text-6xl font-bold">
-          {isLoggedIn ? "Welcome,  User" : " Login"}
+          {isLoggedIn ? "Welcome, User" : "Login"}
         </h1>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+import img from "/herosection.jpg";
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
